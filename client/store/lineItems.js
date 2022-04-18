0d05a65fc056d3760b18fc48ed17ff94669b676a
Chanelle Huang <chanellehuang@gmail.com>
2022-04-17 20:40:31 -0600

    added create thunks to lineItem and Order in store

--- client/store/lineItems.js
+++ client/store/lineItems.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 // ACTION TYPES
 const LOAD_LINEITEMS = 'LOAD_LINEITEMS'
+const CREATE_LINEITEM = 'CREATE_LINEITEM'
 
 // THUNK CREATORS
 export const loadLineItems = () => {
@@ -14,11 +15,23 @@ export const loadLineItems = () => {
   }
 }
 
+export const createLineItem = (status) => {
+  return async (dispatch) => {
+    const lineItem = (await axios.post('/api/lineitems', { quantity })).data
+    dispatch({
+      type: CREATE_LINEITEM,
+      lineItem
+    })
+  }
+}
+
 // REDUCER
 export default function(state = [], action) {
   switch (action.type) {
     case LOAD_LINEITEMS:
       return action.lineItems
+    case CREATE_LINEITEM:
+      return [...state, action.lineItem]
     default:
       return state
   }
--- client/store/orders.js
+++ client/store/orders.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 // ACTION TYPES
 const LOAD_ORDERS = 'LOAD_ORDERS'
+const CREATE_ORDER = 'CREATE_ORDER'
 
 // THUNK CREATORS
 export const loadOrders = () => {
@@ -14,11 +15,23 @@ export const loadOrders = () => {
   }
 }
 
+export const createOrder = (status) => {
+  return async (dispatch) => {
+    const order = (await axios.post('/api/orders', { status })).data
+    dispatch({
+      type: CREATE_ORDER,
+      order
+    })
+  }
+}
+
 // REDUCER
 export default function(state = [], action) {
   switch (action.type) {
     case LOAD_ORDERS:
       return action.orders
+    case CREATE_ORDER:
+      return [...state, action.order]
     default:
       return state
   }
