7259871f0ca1001ece9ad28ad183e4dd448597cc
Chanelle Huang <chanellehuang@gmail.com>
2022-04-21 23:13:15 -1000

    enabled non-logged-in user to add to cart but has errors

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,30 +1,72 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import LineItemInCart from './LineItemInCart';
+import { createLineItem } from '../store/lineItems';
+import { createOrder } from '../store/orders';
 
-const Cart = ({ orders, lineItems }) => {
-  const cart = orders.find(order => order.status === 'cart')
-  if(!cart) return <div>Empty Cart</div> 
+const Cart = ({ auth, orders, lineItems, createLineItem, createOrder }) => {
+  console.log(orders);
+  if (auth.username) {
+    const cart = orders.find(order => order.status === 'cart')
+    if(!cart) return <div>Empty Cart</div>
+  
+    const associatedLineItems = lineItems.filter(lineItem => lineItem.orderId === cart.id)
+  
+    if(!associatedLineItems.length) return <div>Empty Cart</div> 
+  
+    return (
+      <div>
+        <h1>Cart</h1>
+        <ul>
+          {associatedLineItems.map(lineItem => {
+            return (
+              <LineItemInCart lineItem={lineItem} key={lineItem.id} />
+            )
+          })}
+        </ul>
+        <button>Checkout</button>
+      </div>
+    );
+  }
 
-  const associatedLineItems = lineItems.filter(lineItem => lineItem.orderId === cart.id)
+  else {
+    const existingCart = JSON.parse(window.localStorage.getItem('cart'));
 
-  if(!associatedLineItems.length) return <div>Empty Cart</div> 
+    if(!Object.keys(existingCart).length) return <div>Empty Cart</div>
 
-  return (
-    <div>
-      <h1>Cart</h1>
-      <ul>
-        {associatedLineItems.map(lineItem => {
-          return (
-            <LineItemInCart lineItem={lineItem} key={lineItem.id} />
-          )
-        })}
-      </ul>
-      <button>Checkout</button>
-    </div>
-  );
+    for (const productId in existingCart) {
+      createLineItem(+existingCart[productId], productId, null);
+    }
+
+    const associatedLineItems = lineItems.filter(lineItem => !lineItem.orderId)
+
+    return (
+      <div>
+        <h1>Cart</h1>
+        <ul>
+          {associatedLineItems.map(lineItem => {
+            return (
+              <LineItemInCart lineItem={lineItem} key={lineItem.id} />
+            )
+          })}
+        </ul>
+        <button>Checkout</button>
+      </div>
+    );
+  }
 };
 
 const mapState = (state) => state;
 
-export default connect(mapState)(Cart);
+const mapDispatch = (dispatch) => {
+  return {
+    createLineItem: (quantity, productId, orderId) => {
+      dispatch(createLineItem(quantity, productId, orderId))
+    },
+    createOrder: (status) => {
+      dispatch(createOrder(status))
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(Cart);
