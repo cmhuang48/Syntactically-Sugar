6a81c55da835e279a07dce8f8fdcfc44f3e736a4
Lauren Cagnetta <laurenscolleges@gmail.com>
2022-05-03 10:57:43 -0400

    little formatting change on forms

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -39,37 +39,37 @@ class UserProfile extends React.Component {
         <h1 className="font-effect-shadow-multiple">{username[0].toUpperCase() + username.slice(1)}'s info</h1>
         <div className="column1">
         <div className="info">
-        <p>User Name: {username}</p>
-        <p>First Name: {firstName}</p>
-        <p>Last Name: {lastName}</p>
-        <p>Address 1: {address1}</p>
-        <p>Address 2: {address2}</p>
-        <p>City: {city}</p>
-        <p>State: {state}</p>
-        <p>Zip: {zip}</p>
-        <p>Country: {country}</p>
+        <p>User Name:{username}</p>
+        <p>First Name:{firstName}</p>
+        <p>Last Name:{lastName}</p>
+        <p>Address 1:{address1}</p>
+        <p>Address 2:{address2}</p>
+        <p>City:{city}</p>
+        <p>State:{state}</p>
+        <p>Zip:{zip}</p>
+        <p>Country:{country}</p>
         </div>
         </div>
         <div className="borderinfo">
         <form onSubmit={onSubmit}>
         <p> Username:
-          <input name='username' value={username} placeholder='Enter username' onChange={onChange} /></p>
+          <input name='username' value={username} placeholder='Enter username' onChange={onChange} className = "input1" /></p>
         <p> First Name: 
-          <input name='firstName' value={firstName} placeholder='Enter first name' onChange={onChange} /></p>
+          <input name='firstName' value={firstName} placeholder='Enter first name' onChange={onChange} className = "input1" /></p>
         <p>Last Name:
-          <input name='lastName' value={lastName} placeholder='Enter lastName' onChange={onChange} /></p>
+          <input name='lastName' value={lastName} placeholder='Enter lastName' onChange={onChange} className = "input1" /></p>
           <p>Address:
-          <input name='address1' value={address1} placeholder='Enter address1' onChange={onChange} /></p>
+          <input name='address1' value={address1} placeholder='Enter address1' onChange={onChange} className = "input1"/></p>
         <p> Address 2:
-          <input name='address2' value={address2} placeholder='Enter address2' onChange={onChange} /></p>
+          <input name='address2' value={address2} placeholder='Enter address2' onChange={onChange} className = "input1"/></p>
           <p> City:
-          <input name='city' value={city} placeholder='Enter city' onChange={onChange} /></p>
+          <input name='city' value={city} placeholder='Enter city' onChange={onChange} className = "input1"/></p>
         <p> State:
-          <input name='state' value={state} placeholder='Enter state' onChange={onChange} /></p>
+          <input name='state' value={state} placeholder='Enter state' onChange={onChange} className = "input1"/></p>
         <p> Zip:
-          <input name='zip' value={zip} placeholder='Enter zip' onChange={onChange} /></p>
+          <input name='zip' value={zip} placeholder='Enter zip' onChange={onChange} className = "input1"/></p>
         <p> Country:
-          <input name='country' value={country} placeholder='Enter country' onChange={onChange} /></p>
+          <input name='country' value={country} placeholder='Enter country' onChange={onChange} className = "input1" /></p>
         </form>
         <br />
         <button> Update </button>
