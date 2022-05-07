22d558b884f2b0eb0d6af163ad4d594a9a11ed97
yahsuanlinus <savannahwang75@gmail.com>
2022-05-07 11:12:03 -0400

    stripe

--- server/api/stripe.js
+++ server/api/stripe.js
@@ -1,17 +1,65 @@
 const router = require("express").Router();
-const secretKey = require("../../secret");
-const stripe = require("stripe")(secretKey);
+const cors = require("cors");
+const express = require('express')
+const uuid = require("uuid").v4;
+const stripe = require("stripe")(
+  "sk_test_51KwnUeGncdLk4YDTWSKiOvmjkAwiaoHDSAyD7LdUdkEzT3dKFgdlocooR1zt0CIDmZSulwev26xHfGXcsyyDfp2O00vkosWJZF"
+);
+
+router.use(express.urlencoded({ extended: true }));
+router.use(cors());
 
 router.post("/checkout", async (req, res) => {
-  const { token, currency, price } = req.body;
-  let { status } = await stripe.charges.create({
-    amount: price,
-    currency: "usd",
-    source: token,
-  });
-  res.status(200).json({ status });
+  let error;
+  let status;
+  try {
+  const {token, total} = req.body
+    const idempotencyKey = uuid();
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
 
-  if (!{ status }) throw new Error("charge unsuccessful");
+    const charge = await stripe.charges.create(
+      {
+        amount: total*100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip,
+          },
+        },
+      },
+      {
+        idempotencyKey,
+      }
+    );
+    const coupon = await stripe.coupons.create({percent_off: 20, duration: 'once'});
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        discounts: [{
+          coupon: 'promo_1KwoUvGncdLk4YDTZpGroDpD',
+        }],
+        success_url: 'https://example.com/success',
+        cancel_url: 'https://example.com/cancel',
+    });
+    console.log("Charge:", {coupon})
+    //console.log("Charge:", { charge });
+    status = "success";
+  } catch (err) {
+    console.log(err);
+    status = "failure";
+  }
+
+  res.json({ error, status });
 });
 
 module.exports = router;
