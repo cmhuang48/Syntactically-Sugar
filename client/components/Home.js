d736f14dd78a86131674a8cd4df08ab72dea034d
Jiayu Zhang <jiayuzhangg@gmail.com>
2022-05-12 01:37:46 -0400

    commit

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import {OurFavorites} from './Carousel.js'
-import Testimonials, {SimpleRating} from "./Testimonials.js";
+import Testimonials from "./Testimonials.js";
 
 /**
  * COMPONENT
